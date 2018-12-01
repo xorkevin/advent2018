01493e662d76f42d371ebcddd1846d274cfb7260
Kevin Wang <wangkevin448@gmail.com>
2018-11-30 21:18:18 -0800

    Finish day01

--- day01/main.go
+++ day01/main.go
@@ -23,16 +23,36 @@ func main() {
 		}
 	}()
 
+	numlist := []int{}
+	incr := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		num, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(num)
+		numlist = append(numlist, num)
+		incr += num
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+
+	fmt.Println(incr)
+
+	visited := map[int]struct{}{}
+	visited[0] = struct{}{}
+
+	current := 0
+	for {
+		for _, i := range numlist {
+			current += i
+			if _, ok := visited[current]; ok {
+				fmt.Println(current)
+				return
+			}
+			visited[current] = struct{}{}
+		}
 	}
 }
