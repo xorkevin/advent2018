c88e6afa8d39f280be049a75a2bfd6199d5e2a5a
Kevin Wang <wangkevin448@gmail.com>
2018-12-10 22:49:52 -0800

    Finish day11

---
+++ day11/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	puzzleInput = 9810
+
+	boardSize = 300
+)
+
+func powerLevel(x, y int) int {
+	rackID := x + 10
+	return (rackID*y+puzzleInput)*rackID/100%10 - 5
+}
+
+func power9(x, y int, board [][]int) int {
+	power := 0
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			power += board[y+i][x+j]
+		}
+	}
+	return power
+}
+
+func generatePower() [][]int {
+	board := make([][]int, boardSize)
+	for i := range board {
+		board[i] = make([]int, boardSize)
+	}
+	for y, i := range board {
+		for x, _ := range i {
+			board[y][x] = powerLevel(x+1, y+1)
+		}
+	}
+	return board
+}
+
+func generatePartials(board [][]int) [][]int {
+	partial := make([][]int, boardSize+1)
+	for i := range partial {
+		partial[i] = make([]int, boardSize+1)
+	}
+	for i := 0; i < boardSize+1; i++ {
+		partial[0][i] = 0
+		partial[i][0] = 0
+	}
+	for y := 1; y < boardSize+1; y++ {
+		for x := 1; x < boardSize+1; x++ {
+			partial[y][x] = board[y-1][x-1] + partial[y][x-1] + partial[y-1][x] - partial[y-1][x-1]
+		}
+	}
+	return partial
+}
+
+func main() {
+	board := generatePower()
+	{
+		maxPower := 0
+		maxX := 0
+		maxY := 0
+		for y := 0; y < boardSize-2; y++ {
+			for x := 0; x < boardSize-2; x++ {
+				power := power9(x, y, board)
+				if power > maxPower {
+					maxPower = power
+					maxX = x + 1
+					maxY = y + 1
+				}
+			}
+		}
+		fmt.Println(maxX, maxY)
+	}
+
+	partial := generatePartials(board)
+	{
+		maxPower := 0
+		maxX := 0
+		maxY := 0
+		maxSize := 0
+		for i := 1; i < boardSize+1; i++ {
+			for y := 1; y < boardSize-i+1; y++ {
+				for x := 1; x < boardSize-i+1; x++ {
+					power := partial[y+i][x+i] - partial[y-1][x+i] - partial[y+i][x-1] + partial[y-1][x-1]
+					if power > maxPower {
+						maxPower = power
+						maxX = x
+						maxY = y
+						maxSize = i
+					}
+				}
+			}
+		}
+		fmt.Println(maxPower, maxX, maxY, maxSize)
+	}
+}
